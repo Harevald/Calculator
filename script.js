bb558b4b7de173b4f9181fddd84f86a5d57c4be3
Harevald <jakubkrasuski3@gmail.com>
2022-12-01 18:03:59 +0100

    Added operate function

--- script.js
+++ script.js
@@ -13,8 +13,27 @@ function divide(a,b){
     else
         return a/b;
 }
+function operate(key, a, b){
+    if(key==="+"){
+        return add(a,b);
+    }
+    else if(key==="-"){
+        return subtract(a,b);
+    }
+    else if(key==="*"){
+        return multiply(a,b);
+    }
+    else if(key==="/"){
+        return divide(a,b);
+    }
+}
 let a = 7, b = -2;
 console.log(add(a,b));
 console.log(subtract(a,b));
 console.log(multiply(a,b));
 console.log(divide(a,b));
+
+console.log(operate("+", a,b));
+console.log(operate("-", a,b));
+console.log(operate("*", a,b));
+console.log(operate("/", a,b));
